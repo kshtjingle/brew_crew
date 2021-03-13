6037a762b82f086fc764ab192f8cd1f3f729630d
kshtjingle <58187077+kshtjingle@users.noreply.github.com>
2021-03-13 18:44:17 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/user.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        theme: ThemeData(
+          textButtonTheme: TextButtonThemeData(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith((states) => Colors.black),
+            )
+          )
+        ),
+        home: Wrapper(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,108 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+
+class Register extends StatefulWidget {
+
+  final Function toggleView;
+  Register({this.toggleView});
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return loading ? Loading() : Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign up to Brew Crew'),
+        actions: [
+          TextButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign In'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
+      ),
+      body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                  validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.pink,
+                  ),
+                  child: Text('Register', style: TextStyle(color: Colors.white),),
+                  onPressed: () async {
+                    if(_formKey.currentState.validate()){
+                      setState(() {
+                        loading = true;
+                      });
+                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      if(result == null){
+                        setState(() {
+                          error = 'Please supply a valid email';
+                          loading = false;
+                        });
+                      }
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(error, style: TextStyle(color: Colors.red, fontSize: 14),)
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
